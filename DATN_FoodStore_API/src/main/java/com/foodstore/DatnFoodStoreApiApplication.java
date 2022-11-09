4005627c7fc9c656a11b46561a2a4603a798df0c
Kaykaku <90887094+Kaykaku@users.noreply.github.com>
2022-11-10 01:41:29 +0700

    Create SpringBoot App Maven

--- DATN_FoodStore_API/src/main/java/com/foodstore/DatnFoodStoreApiApplication.java
+++ DATN_FoodStore_API/src/main/java/com/foodstore/DatnFoodStoreApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatnFoodStoreApplication {
+public class DatnFoodStoreApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DatnFoodStoreApplication.class, args);
+		SpringApplication.run(DatnFoodStoreApiApplication.class, args);
 	}
 
 }
--- DATN_FoodStore_API/src/main/java/com/foodstore/ServletInitializer.java
+++ DATN_FoodStore_API/src/main/java/com/foodstore/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DatnFoodStoreApplication.class);
+		return application.sources(DatnFoodStoreApiApplication.class);
 	}
 
 }
