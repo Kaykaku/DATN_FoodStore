fd94130cc175fce5206f381b02e511a6c17339fd
Kaykaku <90887094+Kaykaku@users.noreply.github.com>
2022-11-10 00:40:26 +0700

    Create SpringBoot App

---
+++ DATN_FoodStore_ecommerce_website/src/main/java/com/foodstore/DatnFoodStoreApplication.java
@@ -0,0 +1,13 @@
+package com.foodstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnFoodStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnFoodStoreApplication.class, args);
+	}
+
+}
---
+++ DATN_FoodStore_ecommerce_website/src/test/java/com/foodstore/DatnFoodStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnFoodStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
