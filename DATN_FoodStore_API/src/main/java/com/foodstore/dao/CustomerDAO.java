dcda26d2fd73849fb304d4e133aafc089d99cf5c
duyphuong220502 <phuongduy220502@gmail.com>
2022-11-14 22:12:57 +0700

    Update DAO

--- DATN_FoodStore_API/src/main/java/com/foodstore/dao/CustomerDAO.java
+++ DATN_FoodStore_API/src/main/java/com/foodstore/dao/CustomerDAO.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.foodstore.model.entity.Customer;
 
 @Repository
 public interface CustomerDAO extends JpaRepository<Customer, Long> {
+
+	@Query("SELECT c FROM Customer c WHERE c.is_display = ?1")
 	List<Customer> findByIsDisplay(Boolean isDisplay);
 
+	@Query("SELECT c FROM Customer c WHERE c.is_display = ?1")
 	Page<Customer> findByIsDisplay(Boolean isDisplay, PageRequest pageable);
 }
